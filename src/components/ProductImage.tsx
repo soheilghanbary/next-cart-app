28be5f6772e1879cad1c04860596141b8754ae44
soheilghanbary <soli.ghanbary@gmail.com>
2023-09-08 14:01:46 -0700

    optimaztion images 2

--- src/components/ProductImage.tsx
+++ src/components/ProductImage.tsx
@@ -13,12 +13,10 @@ export const ProductImage = ({ image = "", alt = "" }) => {
         fill
         alt={alt}
         src={image}
-        sizes="(min-width: 60em) 24vw,
-        (min-width: 28em) 45vw,
-        100vw"
+        sizes="(min-width: 1520px) 230px, (min-width: 1280px) calc(10.91vw + 66px), (min-width: 1040px) calc(25vw - 50px), (min-width: 780px) calc(33.33vw - 50px), calc(50vw - 50px)"
         loading="lazy"
         className={cn(
-          "rounded-[inherit] object-cover duration-700 ease-in-out",
+          "h-auto rounded-[inherit] object-cover duration-700 ease-in-out",
           loading ? "blur-xl" : "blur-0"
         )}
         onLoadingComplete={() => setLoading(false)}
